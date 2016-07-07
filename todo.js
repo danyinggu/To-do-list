960b0b8d472244c86609f3c66676c5480d5bc2bc
danyinggu <danying.gu@kik.com>
2016-07-06 21:50:39 -0400

    line

--- todo.js
+++ todo.js
@@ -1,11 +1,37 @@
 //todo.js
 
+function updateStatus() {
+    var cbId = this.id.replace("cb_", "");
+    var itemInd = document.getElementById("item_" + cbId);
+  
+    if (this.checked){ 
+      itemInd.style.textDecoration = "line-through";
+      itemInd.style.color="red";
+      
+      itemInd.style.fontStyle="alias";
+      itemInd.style.fontSize="0.9em";
+      //itemInd.classname="checked"; 
+      // in .css, I can add .checked { //cssStyle }
+    } else {
+      itemInd.style.textDecoration = "none";
+      itemInd.style.color="black";
+      itemInd.style.fontWeight="bold";
+
+      // itemInd.classname="";
+    }
+}
+
 function addItems(itemName,List){
+   totalNum++;
+
    var Item = document.createElement("li");   
    var checkbox = document.createElement("input");
    checkbox.type = "checkbox";
+   checkbox.id = "cb_" + totalNum;
+   checkbox.onclick = updateStatus;
 
    var span = document.createElement("span");
+   span.id = "item_" + totalNum;
    span.innerText = itemName;
 
    Item.appendChild(checkbox);
@@ -15,8 +41,7 @@ function addItems(itemName,List){
    
 
 }
-
-
+var totalNum= 0;
 var inlineText = document.getElementById("inline-text");
 inlineText.focus();
 
