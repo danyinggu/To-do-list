d22fbcd72af9bd91d691e5e6db32858e3af86fbe
danyinggu <danying.gu@kik.com>
2016-07-25 19:53:37 -0400

    basically have mutiple to do list

--- todo.js
+++ todo.js
@@ -21,7 +21,6 @@ function updateStatus() {
       itemInd.style.textDecoration = "none";
       itemInd.style.fontStyle="";
       itemInd.style.color="black";
-      itemInd.style.fontWeight="bold";
       itemInd.style.fontSize="1em";
 
       // itemInd.classname="";
@@ -62,29 +61,35 @@ var totalNum = 0;
 var totalChecked = 0;
 
 // make the cursor focus on the field
-var inlineText = document.getElementById("inline-text");
-inlineText.focus();
+//var inlineText = document.getElementById("inlinetext");
+//inlineText.focus();
+//var addItem = document.getElementById("buttonNew");
 
-var addItem = document.getElementById("buttonNew");
-
-inlineText.onkeyup = function(eve) {
+function itemEnter (eve){
+  var inlineText = this.id;
    if(eve.which == 13) {
      var itemName = inlineText.value;
     }
+
     if((!itemName) || (itemName == "")) {
           return false;
-        }
+    }
+
+    var listID = this.id.replace("inline-text_", "");
+    var thisList = document.getElementById("todolist"+listID);
+    console.log(itemName);
 // add items (call function)
-       addItems(itemName, document.getElementById("todolist"));
+       addItems(itemName, thisList);
 
 // after adding items, select the previous field and focus them
          inlineText.value="";
+}
 
-};
 
 // once click the button, add text to according to items
-addItem.onclick = function() {
-
+function addItem() {
+       var listID = this.id.replace("buttonNew_", "");
+       var inlineText = document.getElementById("inline-text_" + listID);
        var itemName = inlineText.value;
 
 //if user enters space or nothing
@@ -92,9 +97,79 @@ addItem.onclick = function() {
           return false;
         }
 
+        var thisList = document.getElementById("todolist_"+listID);
+
+        console.log(itemName);
+
 // add items (call function)
-       addItems(itemName, document.getElementById("todolist"));
+       addItems(itemName, thisList);
+
 
 // after adding items, select the previous field and focus them
          inlineText.value="";
-     };
+         inlineText.focus();
+
+};
+
+
+
+
+
+var totalListNum = 0;
+
+var addList = document.getElementById("addButton");
+
+addList.onclick = function() {
+   var listName = prompt("Enter the name of the new list:");
+   console.log("1");
+     if((!listName) || (listName == "")) {
+          return false;
+        }
+
+   totalListNum++;
+   // print the list Name on the page
+   var newList = document.createElement("P");
+   var newListName=document.createTextNode(listName);
+   newList.style.fontWeight = "bold";
+   newList.style.fontSize = "1.5em";
+   newList.appendChild(newListName);
+   document.body.appendChild(newList);
+
+   // add the text field of item of this list
+   var inputLine = document.createElement("P");
+   var textField = document.createElement("input");
+   textField.type="text";
+   textField.id="inline-text_" + totalListNum;
+   textField.className="inlinetext";
+
+   // add the button for adding new items of this list
+   var itemButton = document.createElement("button");
+   var buttonText = document.createTextNode("New Item");
+   itemButton.id = "buttonNew_" + totalListNum;
+   itemButton.style.marginLeft = "5px";
+   itemButton.className="newButton";
+   itemButton.appendChild(buttonText);
+
+   // put the text field and list to the page
+   inputLine.appendChild(textField);
+   inputLine.appendChild(itemButton);
+   document.body.appendChild(inputLine);
+   console.log("2");
+
+   // add the item ul following the text field and button
+   var listOfItem = document.createElement("ul");
+   listOfItem.id = "todolist_" + totalListNum;
+   document.body.appendChild(listOfItem);
+
+
+    // define the function onkeyup for the textField
+    textField.onkeyup = itemEnter;
+   // define the function onClick when any new button clicked
+   itemButton.onclick = addItem;
+
+   console.log("3");
+
+   
+   
+};
+  
